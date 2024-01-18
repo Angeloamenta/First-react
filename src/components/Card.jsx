2e5671890c2c7e519faac8f4993a5b1b0fecd309
Angeloamenta <angeloamenta7gmail.com>
2024-01-18 14:05:57 +0100

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+function Card({title, imgUrl, isVisited}) {
+
+
+
+    return (
+        <div className="bg-gray-800 rounded">
+            <div className="h-40 w-full">
+            <img src={imgUrl} alt="" className="w-full h-full object-cover" />
+            </div>
+            <h2 className="mt-3 max-h-3 font-bold">{title}</h2>
+            <p className="p-3 min-h-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus, ex cumque modi incidunt dolore eius.</p>
+            {isVisited && <span>si</span>}
+            {!isVisited && <span>no</span>}
+
+        </div>
+    )
+}
+
+export default Card
